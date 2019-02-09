a5b9763ee0273a4ff89005e0dc16dac3146418fb
Yashwanth1988 <bathiniyashwanth@gmail.com>
2019-02-09 08:08:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class App extends React.Component {
+
+
+    render(){
+        return (
+            <div>
+                <h1>Hi This is the Yashwanth.</h1>
+            </div>
+        )
+    }
+
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+
+    entry : './src/index.js',
+    output : {
+        path : path.join(__dirname, '/dist'),
+        publicPath: '/dist/',
+        filename : 'bundle.js'
+    },
+
+    devtool: 'eval-source-map',
+
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
+            loader: "babel-loader",
+            options: { presets: ["@babel/env", "@babel/preset-react"] }
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+          }
+        ]
+      },
+
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+
+
+}
