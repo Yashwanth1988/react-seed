cf60f73d72ad3c069b92a189f3bd47529264f08f
Yashwanth1988 <bathiniyashwanth@gmail.com>
2019-02-09 09:49:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 export class App extends React.Component {
 
-
+    
     render(){
+       
         return (
             <div>
-                <h1>Hi This is the Yashwanth.</h1>
+                <h1>Hi {this.props.message}, to the React world.</h1>
             </div>
         )
     }
